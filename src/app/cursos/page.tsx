10550ab0a41a9086aa26dfd3d59fc9ebf7320ec3
rafaelcesar0 <rafaelcesar0@proton.me>
2024-07-15 11:48:06 -0300

    feat: add navigation drawer

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CodarSe - Todos os cursos",
+};
+
 export default function PageCursos() {
   return (
     <main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CodarSe - Página inicial",
+};
+
+
+
 export default function Home() {
   return (
     <main>
